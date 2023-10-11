06be565bb5e9e944ef61afef299d07c61461fafb
Bigbrainium <prashunregmi15@gmail.com>
2023-10-11 09:32:30 -0400

    Lab_06_Part_01

--- src/TempCToF.java
+++ src/TempCToF.java
@@ -1,7 +1,29 @@
 import java.util.Scanner;
 
-public class Main {
+public class TempCToF {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        double tempC = 0.00;
+        double tempF;
+        boolean valid = false;
+
+        System.out.println("Input the temperature in Celsius");
+        do {
+            if (scan.hasNextDouble()) {
+                valid = true;
+            }
+            else {
+                scan.nextLine();
+                System.out.println("Please enter a valid input");
+            }
+
+
+        } while (valid == false);
+
+        tempC = scan.nextDouble();
+        tempF = tempC * 9 / 5 + 32;
+        System.out.printf("%-7s %7.2f", "Degrees C:", tempC);
+        System.out.printf("\n%-9s %7.2f", "Degrees F:", tempF);
     }
 }
